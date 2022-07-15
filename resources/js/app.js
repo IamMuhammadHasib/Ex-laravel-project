564d49684580f8f794c0bc9a21468f6509ecd925
mohammadFromBengal <mohammadfrombengal@gmail.com>
2022-07-16 01:39:14 +0600

    collaborator

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+const hamburger = document.getElementById("hamburger");
+const nav_link = document.getElementById("nav_links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  nav_link.classList.toggle("active");
+});
+
+console.log("demo js file working....................");
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,159 +1,139 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="css/styles.css"> -->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>AmarVasha</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;1,500&display=swap"
+    rel="stylesheet"
+    />
     
+    <link rel="stylesheet" href="./resources/css/index.css" />
+    <link rel="stylesheet" href="./resources/css/query.css" />
+
     @vite(['resources/css/app.css','resources/css/query.css', 'resources/js/app.js'])
-</head>
+  </head>
 
-<body style="background-color:#f8fafc;">
-    <nav class="navbar navbar-expand-md shadow-sm p-0 mb-5 bg-white rounded">
-
-        <a class="navbar-brand" href="#">
-            <img style="margin-left: 2rem;" src="images/logo.png" width="100" height="auto" alt="">
-        </a>
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-current="page" href="#">Why Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-current="page" href="#">Curriculum</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-current="page" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <button type="button" class="btn1">Login</button>
-                </li>
-                <li class="nav-item">
-                    <button type="button" class="btn2">Get Free Trial</button>
-                </li>
-            </ul>
+  <body>
+    <nav>
+      <div class="container">
+        <div class="img_logo">
+          <img src="img/logo.png" alt="logo" />
         </div>
+        <div class="nav_links" id="nav_links">
+          <ul class="nav_menu">
+            <li class="nav_link">
+              <a href="">Why Us </a>
+            </li>
+            <li class="nav_link">
+              <a href="">Criculam </a>
+            </li>
+            <li class="nav_link">
+              <a href="">pricing </a>
+            </li>
+            <li class="nav_login nav_link >
+              <a href="">Login </a>
+            </li>
+            <li class="nav_trial nav_link">
+              <a href="">Get Free Trial </a>
+            </li>
+          </ul>
+          <div class="hamburger" id="hamburger">
+            <span class= "bar"></span><span class="bar">
+
+            </span><span class="bar"></span>
+          </div>
+        </div>
+      </div>
     </nav>
-
-    <div class="container" style="margin-top: -5px;">
-        <div class="row leftSection">
-            <div class="col-7">
-                <h2>Membership</h2>
-                <p class="subtitle">Make your own plan and stay connected with Amar Bhasa Family.</p>
-                <button type="button" class="btn3">Prepaid</button>
-                <button type="button" class="btn4">Postpaid</button>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-5" style="padding-left: 0rem;">
-                            <h3>Class per week</h3>
-                            <p class="description">3 classes per week</p>
-                        </div>
-                        <div class="col-7">
-                            <button type="button" class="btnC">1</button>
-                            <button type="button" class="btnC">2</button>
-                            <button type="button" class="btnC" style="background: #f58120; color:#ffffff;">3</button>
-                            <button type="button" class="btnC">4</button>
-                            <button type="button" class="btnC">5</button>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-5" style="padding-left: 0rem;">
-                            <h3>Number of month For membership</h3>
-                            <p class="description">0 month membership</p>
-                        </div>
-                        <div class="col-7">
-                            <button type="button" class="btnC">1</button>
-                            <button type="button" class="btnC">2</button>
-                            <button type="button" class="btnC">3</button>
-                            <button type="button" class="btnC" style="color: #8ea0a1">4</button>
-                            <button type="button" class="btnC" style="color: #8ea0a1">5</button>
-                            <button type="button" class="btnC" style="color: #8ea0a1">6</button>
-                            <button type="button" class="btnC" style="color: #8ea0a1">7</button>
-                            <button type="button" class="btnC" style="color: #8ea0a1">8</button>
-                            <button type="button" class="btnC" style="color: #8ea0a1">9</button>
-                            <button type="button" class="btnC" style="color: #8ea0a1; padding: 8px 12px">10</button>
-                            <button type="button" class="btnC" style="color: #8ea0a1; padding: 8px 12px">11</button>
-                            <button type="button" class="btnC" style="color: #8ea0a1; padding: 8px 12px">12</button>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col" style="padding-left: 0;">
-                            <input type="checkbox" name="check" value="Yes">
-                            <label for="check"> Turn on auto-renewal</label><br>
-                        </div>
-                    </div>
-                </div>
-
-
+    <main>
+      <div class="main_container">
+        <div class="left_section">
+          <div class="Membership">
+            <h2>Membership</h2>
+            <p>Make your own plan and stay connectd with Amar Vasha Family</p>
+            <div class="options">
+              <label for="">Prepaid</label>
+              <label for="">Postpaid</label>
             </div>
-            <div class="col-1">
-                <div class="vl"></div>
+          </div>
+          <div class="class_per_week">
+            <div class="week_title">
+              <h2>Class per week</h2>
+              <p>3 classes per week</p>
             </div>
-            <div class="col">
-                <div class="container rightSection">
-                    <div class="row">
-                        <div class="col" style="padding-left: 0rem;">
-                            <p>Class per week</p>
-                        </div>
-                        <div class="col" style="padding-right: 0rem">
-                            <p class="right">3</p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding-bottom: 2px;">
-                        <div class="col-2" style="padding-left: 0rem;">
-                            <p style="line-height: 100%;">Membership months</p>
-                        </div>
-                        <div class="col" style="padding-right: 0rem">
-                            <p class="right">0</p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="hl"></div>
-                    </div>
-                    <div class="row ">
-                        <p class="price">0$</p>
-                        <p class="perPrice">Per class</p>
-                        <div class="container" style="padding-bottom: 0.5rem;">
-                            <div class="row">
-                                <div class="col" style="padding: 0">
-                                    <p>Total class</p>
-                                </div>
-                                <div class="col" style="padding: 0">
-                                    <p class="right">0$</p>
-                                </div>
-                            </div>
-                        </div>
-                        <p style="padding: 0; line-height: 1rem;">If you click on this button you will be redirect to
-                            2checkout payment gateway</p>
-                        <button type="button" class="block btnPay">Pay in USD</button>
-                    </div>
-                </div>
+            <div class="class_days">
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+              </ul>
             </div>
+          </div>
+          <div class="Number_of_months">
+            <div class="number_title">
+              <h2>Number of month For membership</h2>
+              <p>0 month membership</p>
+            </div>
+            <div class="number_options">
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
+                <li>8</li>
+                <li>9</li>
+                <li>10</li>
+                <li>11</li>
+                <li>12</li>
+              </ul>
+            </div>
+          </div>
+          <div class="renewale">
+            <input type="checkbox" value="check" />
+            <label for="">Turn ON Auto Renewal</label>
+          </div>
         </div>
-    </div>
 
-    <button type="button" class="btnR"><img src="images/rotate-right-solid.svg" width="20" height="auto"
-            alt=""></button>
-    <p class="footer">Amar Vasha is a service by Tinkers Technologies Ltd</p>
+        <div class="mid_section">
+          <hr class="divider">
+        </div>
+        <div class="right_section">
+          <div class="informations">
+            <div class="class_info"><p>Class per week</p>
+            <p>3</p></div>
+            <div class="membership_info">
+              <p>Membership <br> months</p>
+            <p>0</p>
+            </div>
+            <div class="divider2"></div>
+            <div class="currency">
+              <p>0$</p>
+              <h4>Per class</h4>
+            </div>
+            <div class="pay_container">
+              <div class="total">
+                <p>Total</p>
+                <p>0 <span>$</span></p>
+              </div>
+              <p>If you click on this button you will be redirected to checkout payment gateway</p>
+              <button class="btn"type="submit">Pay in USD</button>
+            </div>
+            <div class="divider2"></div>
+        </div>
+      </div>
+    </main>
 
-</body>
-
+    <!-- javascript  -->
+    <script src="./resources/js/index.js"  ></script>
+  </body>
 </html>
